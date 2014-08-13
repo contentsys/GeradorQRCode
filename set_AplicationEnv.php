4635f01ce7c6b98ae5451849fea4a18784c2debc
luan <luan@luan-pc>
2014-08-13 16:46:28 -0300

    Aplication Enviroments

--- set_AplicationEnv.php
+++ set_AplicationEnv.php
@@ -1,8 +1,8 @@
 <?php
-define("APP_NAME", "scm");
+define("APP_NAME", "GeradorQRCode");
 define("APP_SRC", "./");
-define("APPLICATION_ENV", "production");
-define("DEBUG_MODE", false);
+define("APPLICATION_ENV", "development");
+define("DEBUG_MODE", true);
 define("ROOTFOLDER", dirname(__FILE__));
 define("RESOURCE_FOLDER", $_SERVER["DOCUMENT_ROOT"] . "/".APP_NAME. "/" . APP_SRC . "/resources/");
 define("UI_FOLDER", "/".APP_NAME. "/" . APP_SRC . "/ui/");
