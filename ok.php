d87265b5685fb635e5f4dc0882dd0191c452706f
lucas <lucas@debian>
2014-08-14 10:27:37 -0300

    ok

---
+++ ok.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<title>Solicitacao enviada com sucesso!</title></head>
+<body bgcolor="#FF9318">
+<div align="center" style="margin-top: 10%;">
+	<strong>Solicita&ccedil;&atilde;o enviada com sucesso!</strong><br>
+	<img src="juiceintime.png"><br>
+	<strong>Obrigado</strong>
+</div>
+</body>
+</html>
